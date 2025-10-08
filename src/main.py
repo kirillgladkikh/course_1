cd58f5a564e9f352e4d4e3fdfc57810c7830921e
Kirill Gladkikh <k.gladkikh@mail.ru>
2025-10-08 17:36:49 +0300

    func read_transactions_from_excel (xlsx to List[Dict])

--- src/main.py
+++ src/main.py
@@ -1,6 +1,19 @@
+import os
+import json
 # from src.views import
 # from src.services import
 # from src.reports import
+from src.utils import read_transactions_from_excel
+
+
+transactions = read_transactions_from_excel("data/operations.xlsx")
+
+# # Преобразуем в JSON строку
+# json_data = json.dumps(transactions, ensure_ascii=False, indent=4)
+#
+# # Сохраняем в файл
+# with open('transactions.json', 'w', encoding='utf-8') as file:
+#     file.write(json_data)
 
 
 def views_main():
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,120 @@
+import os
+import pandas as pd
+from typing import Dict, List
 from datetime import datetime
 
-# Чтение из XLS-файла в JSON
+# Чтение из XLS-файла в список словарей
+def read_transactions_from_excel(file_path: str = "data/operations.xlsx") -> List[Dict]:
+    """
+    Считывает финансовые операции из Excel-файла.
+    Args: file_path (str): путь к Excel-файлу
+    Returns: List[Dict]: список словарей с транзакциями
 
+    Описание данных:
+    transaction_date - Дата операции — дата, когда произошла транзакция.
+    payment_date - Дата платежа — дата, когда был произведен платеж.
+    card_number - Номер карты — последние 4 цифры номера карты.
+    transaction_status - Статус — статус операции (например, OK, FAILED).
+    transaction_amount - Сумма операции — сумма транзакции в оригинальной валюте.
+    transaction_currency - Валюта операции — валюта, в которой была произведена транзакция.
+    payment_amount - Сумма платежа — сумма транзакции в валюте счета.
+    payment_currency - Валюта платежа — валюта счета.
+    cashback_amount - Кешбэк — размер полученного кешбэка.
+    transaction_category - Категория — категория транзакции.
+    transaction_code - MCC — код категории транзакции (соответствует международной классификации).
+    transaction_description - Описание — описание транзакции.
+    total_bonus - Бонусы (включая кешбэк) — количество полученных бонусов (включая кешбэк).
+    invest_amount_rounded - Округление на «Инвесткопилку» — сумма, которая была округлена и переведена на «Инвесткопилку».
+    transaction_amount_rounded - Сумма операции с округлением — сумма транзакции, округленная до ближайшего целого числа.
+    """
+    try:
+        # Получаем путь к корню проекта
+        project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+        # Формируем полный путь
+        full_path = os.path.join(project_root, file_path)
+        print(full_path)
+        # Читаем Excel файл
+        df = pd.read_excel(full_path, engine="openpyxl", header=0)
+
+        # Проверяем, что все необходимые столбцы присутствуют
+        required_columns = [
+            "Дата операции",
+            "Дата платежа",
+            "Номер карты",
+            "Статус",
+            "Сумма операции",
+            "Валюта операции",
+            "Сумма платежа",
+            "Валюта платежа",
+            "Кэшбэк",
+            "Категория",
+            "MCC",
+            "Описание",
+            "Бонусы (включая кэшбэк)",
+            "Округление на инвесткопилку",
+            "Сумма операции с округлением"
+            # "id",
+            # "state",
+            # "date",
+            # "amount",
+            # "currency_name",
+            # "currency_code",
+            # "from",
+            # "to",
+            # "description",
+        ]
+
+        if not all(column in df.columns for column in required_columns):
+            print(required_columns)
+            raise ValueError("Файл Excel не содержит все необходимые столбцы")
+
+        transactions = []
+
+        for _, row in df.iterrows():
+            try:
+                # Преобразуем данные в нужный формат
+                transaction = {
+                    "transaction_date": row["Дата операции"],
+                    "payment_date": row["Дата платежа"],
+                    "card_number": row["Номер карты"],
+                    "transaction_status": row["Статус"],
+                    "transaction_amount": row["Сумма операции"],
+                    "transaction_currency": row["Валюта операции"],
+                    "payment_amount": row["Сумма платежа"],
+                    "payment_currency": row["Валюта платежа"],
+                    "cashback_amount": row["Кэшбэк"],
+                    "transaction_category": row["Категория"],
+                    "transaction_code": row["MCC"],
+                    "transaction_description": row["Описание"],
+                    "total_bonus": row["Бонусы (включая кэшбэк)"],
+                    "invest_amount_rounded": row["Округление на инвесткопилку"],
+                    "transaction_amount_rounded": row["Сумма операции с округлением"]
+                    # "id": row["id"],
+                    # "state": row["state"],
+                    # "date": row["date"],
+                    # "amount": row["amount"],
+                    # "currency_name": row["currency_name"],
+                    # "currency_code": row["currency_code"],
+                    # "from": row["from"],
+                    # "to": row["to"],
+                    # "description": row["description"],
+                }
+
+                transactions.append(transaction)
+
+            except Exception as e:
+                print(f"Ошибка при обработке строки: {row}. Причина: {str(e)}")
+                continue
+
+        return transactions
+
+    except FileNotFoundError:
+        print(f"Ошибка: файл {file_path} не найден")
+        return []
+
+    except Exception as e:
+        print(f"Произошла ошибка при чтении файла: {str(e)}")
+        return []
 
 
 # Утилиты для модуля views.py
