0308baf26978278a283eabfecbe9141e814e33e5
Kirill Gladkikh <k.gladkikh@mail.ru>
2025-10-08 16:20:24 +0300

    main structure

---
+++ src/main.py
@@ -0,0 +1,15 @@
+# from src.views import
+# from src.services import
+# from src.reports import
+
+
+def views_main():
+    pass
+
+
+def services_main():
+    pass
+
+
+def reports_main():
+    pass
